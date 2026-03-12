b1e00d7bb54f1022652d1f10b41d0663d04e97c2
daxieabc <y824855275@gmail.com>
2026-03-12 18:05:01 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    vector<int> nums = {1, 2, 8, 10, 33};
+
+    return 0;
+}
